64de0a013edf108a0515e14b121f01c270386572
M00NBOY <zeroualmahel@gmail.com>
2018-03-06 12:50:41 +0100

    add a lot

--- js/app.js
+++ js/app.js
@@ -3,12 +3,21 @@ var illustration = document.querySelector('.ennemieImg');
 var progressBar = document.querySelector('.ennemieHp');
 var hp = document.querySelector('.actualHp');
 var maxhp = document.querySelector('.maxHp');
+var actualYen = document.querySelector('.yenNb');
+var yen_btn = document.querySelector('.yen_btn');
+var acutal_dpc = document.querySelector('.actualDpc');
+var yenNeeded = document.querySelector('.yenNeeded');
 var counter = 0;
 var big = 0;
 var force = 1;
+var yen = 0;
+var lvl = 1;
+var requireYen = 1;
+var yenExp = 1;
+
+
 
 window.addEventListener('dblclick', function(event){
-    console.log(event);
     event.preventDefault()
 })
 
@@ -19,12 +28,26 @@ ennemie.addEventListener('click', function(){
     if (progressBar.value < 1) {
         counter++;
         changement();
+        gainYen();
         if (counter>4) {
             counter = 0;
             gainHp();
+            lvl++;
         }
     }
-})
+});
+
+yen_btn.addEventListener('click', function(){
+    if (yen >= requireYen) {
+        yen=yen-requireYen;
+        force++;
+        requireYen+= yenExp;
+        yenExp+= 2;
+        actualYen.textContent = " " + yen + "¥";
+        acutal_dpc.textContent = " " + force;
+        yenNeeded.textContent = " " + requireYen + "¥";
+    }
+});
 
 function changement(){
     progressBar.value = progressBar.max;
@@ -39,6 +62,15 @@ function gainHp(){
 }
 
 function randomImg(){
-        var number = Math.floor(Math.random()*8);
+        var number = Math.floor(Math.random()*9);
         illustration.src = 'img/'+ number + '.png';
+}
+
+function gainYen(){
+    yen+=lvl;
+    actualYen.textContent = " " + yen + "¥";
+}
+
+function forceUpdate(){
+
 }
